a43d4578ae55a89740ecbf081eb59d587bf66e48
Tiberiu Feredean <tiberiu.feredean@gmail.com>
2019-09-04 19:50:21 +0300

    - boilerplate

---
+++ src/app/register/register.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegisterComponent } from './register.component';
+
+
+
+@NgModule({
+  declarations: [RegisterComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class RegisterModule { }
